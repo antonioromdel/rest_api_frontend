3b84ce63b83855e96c599b8b1e994812d1f7e9b6
Antonio Roman <antonioromndelgado@gmail.com>
2025-01-22 15:38:49 +0100

    first commit

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -24,50 +24,50 @@ export default function ProductDetails({product}: ProductDetailsProps) {
 
   return (
     <>
-      <tr className="border-b ">
-        <td className="p-3 text-lg text-gray-800 text-center">{product.name}</td>
-        <td className="p-3 text-lg text-gray-800 text-center">{formatCurrency(product.price)}</td>
-        <td className="p-3 text-lg text-gray-800 text-center">
-          <fetcher.Form method="POST">
-              <button
-                type="submit"
-                name="id"
-                value={product.id}
-                className={`${isAvailable ? 'text-white bg-green-600' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
-              >
-              {isAvailable ? 'Disponible' : 'No disponible'}
-              </button>
-          </fetcher.Form>
-
-        </td>
-        <td className="p-3 text-lg text-gray-800 ">
-            <div className="flex gap-2 items-center">
-                <button 
-                  onClick={() => navigate(`producto/${product.id}/editar`)}
-                  className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
-                >Editar</button>
-
-                <Form
-                  className="w-full"
-                  method="POST"
-                  action={`producto/${product.id}/eliminar`}
-                  onSubmit={(e) => {
-                    if( !confirm('¿Eliminar?')){
-                      e.preventDefault()
-                    }
-                  }}
+        <tr className="border-b ">
+          <td className="p-3 text-lg text-gray-800 text-center">{product.name}</td>
+          <td className="p-3 text-lg text-gray-800 text-center">{formatCurrency(product.price)}</td>
+          <td className="p-3 text-lg text-gray-800 text-center">
+            <fetcher.Form method="POST">
+                <button
+                  type="submit"
+                  name="id"
+                  value={product.id}
+                  className={`${isAvailable ? 'text-white bg-green-600' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
                 >
-                  <input 
-                    type="submit"
-                    value="Eliminar producto"
-                    className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:cursor-pointer"
-                  />
-                </Form>
+                {isAvailable ? 'Disponible' : 'No disponible'}
+                </button>
+            </fetcher.Form>
 
-            </div>
+          </td>
+          <td className="p-3 text-lg text-gray-800 ">
+              <div className="flex gap-2 items-center">
+                  <button 
+                    onClick={() => navigate(`producto/${product.id}/editar`)}
+                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                  >Editar</button>
 
-        </td>
-      </tr>
+                  <Form
+                    className="w-full"
+                    method="POST"
+                    action={`producto/${product.id}/eliminar`}
+                    onSubmit={(e) => {
+                      if( !confirm('¿Eliminar?')){
+                        e.preventDefault()
+                      }
+                    }}
+                  >
+                    <input 
+                      type="submit"
+                      value="Eliminar producto"
+                      className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:cursor-pointer"
+                    />
+                  </Form>
+
+              </div>
+
+          </td>
+        </tr>
     </>
   );
 }
--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,17 +1,22 @@
 import { Outlet } from "react-router-dom"
+import Loading from "../components/Loading"
 
 export default function Layouts() {
   return (
     <>
+
+        
         <header className="bg-slate-800">
-            <div className="mx-auto max-w-6xl py-10">
+            <div className="p-10">
                 <h1 className="text-4xl font-extrabold text-white">
                     Administrador de Productos
                 </h1>
             </div>
         </header>
 
-        <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+
+        <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow rounded-lg relative">
+            {/* <Loading /> */}
             <Outlet />
         </main>
     </>
